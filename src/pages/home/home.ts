530e6cf38543c8bfa9bb0731b3baad0655ec1185
xxj2016 <13726224979@163.com>
2018-03-24 09:02:48 +0800

    头部滑动菜单；轮播图

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,13 +6,42 @@ import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  @ViewChild('slider') slider: Slides;
   selectedTheme: String;
+
+    slides = [
+    {
+      title: 'Dream\'s Adventure',
+      imageUrl: 'assets/imgs/lists/wishlist-1.jpg',
+      songs: 2,
+      private: false
+    },
+    {
+      title: 'For the Weekend',
+      imageUrl: 'assets/imgs/lists/wishlist-2.jpg',
+      songs: 4,
+      private: false
+    },
+    {
+      title: 'Family Time',
+      imageUrl: 'assets/imgs/lists/wishlist-3.jpg',
+      songs: 5,
+      private: true
+    },
+    {
+      title: 'My Trip',
+      imageUrl: 'assets/imgs/lists/wishlist-4.jpg',
+      songs: 12,
+      private: true
+    }
+  ];
   constructor(
     public navCtrl: NavController,
     public nanParams: NavParams
   ) {
-    
+    for (let i = 0; i < 20; i++) {
+      this.slides.push(this.slides[i % 4]);
+    }
   }
 
 }
