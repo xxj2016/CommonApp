633f8463d1a92456fa00c9a31b6797e8e66e50dd
xxj2016 <13726224979@163.com>
2018-03-24 09:10:41 +0800

    音乐卡片内容

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,7 +9,7 @@ export class HomePage {
   @ViewChild('slider') slider: Slides;
   selectedTheme: String;
 
-    slides = [
+  slides = [
     {
       title: 'Dream\'s Adventure',
       imageUrl: 'assets/imgs/lists/wishlist-1.jpg',
@@ -35,6 +35,32 @@ export class HomePage {
       private: true
     }
   ];
+  cards = [
+    {
+      imageUrl: 'assets/imgs/card/nin-live.png',
+      title: 'Nine Inch Nails Live',
+      description: 'The most popular industrial group ever, and largely responsible for bringing the music to a mass audience.'
+    },
+    {
+      imageUrl: 'assets/imgs/card/badu-live.png',
+      title: 'Erykah Badu',
+      description: 'American singer-songwriter, record producer, activist, and actress, Badu\'s style is a prime example of neo-soul.'
+    },
+    {
+      imageUrl: 'assets/imgs/card/queen-live.png',
+      title: 'Queen',
+      description: 'The British rock band formed in London in 1970, and is considered one of the biggest stadium rock bands in the world.'
+    },
+    {
+      imageUrl: 'assets/imgs/card/bjork-live.jpg',
+      title: 'Björk',
+      description: 'Björk is an Icelandic singer, songwriter and actress.'
+    },
+    {
+      imageUrl: 'assets/imgs/card/rundmc-live.png',
+      title: 'Run-D.M.C.',
+      description: 'The American hip hop group widely acknowledged as one of the most influential acts in the history of hip hop.'
+    },];
   constructor(
     public navCtrl: NavController,
     public nanParams: NavParams
